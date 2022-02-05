aeeccdeaa558080aa93b26e002a4356c93f96ecc
dyparkkk <dkdlel1z@naver.com>
2022-02-05 13:04:11 +0900

    first commit

---
+++ src/main/java/com/example/woowabackend/WoowaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.woowabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/woowabackend/WoowaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woowabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
