2172efd94d3d415f2df1f75526fba7ba6c4c5609
dyparkkk <dkdlel1z@naver.com>
2022-03-25 00:03:13 +0900

    add test api

--- src/main/java/com/example/woowabackend/member/exception/ErrorResponse.java
+++ src/main/java/com/example/woowabackend/member/exception/ErrorResponse.java
@@ -6,6 +6,20 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class ErrorResponse {
-    private String code;
-    private String message;
+    private boolean success;
+    private Error error;
+
+    public static ErrorResponse createErrorResponse(boolean success, String code, String message) {
+        Error error = new Error(code, message);
+        return new ErrorResponse(success, error);
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class Error{
+        private String code;
+        private String message;
+    }
 }
+
+
